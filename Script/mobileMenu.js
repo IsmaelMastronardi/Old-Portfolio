23fdf0d4c0b85663101414aa6676234ddd272057
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-06-19 14:41:28 -0300

    add script link in html file, add event listener to mobileMenu.js

---
+++ Script/mobileMenu.js
@@ -0,0 +1,23 @@
+const hamburgerButton = document.querySelector('#mobileMenuBtn');
+
+function createList(menu) {
+  const menuList = document.createElement('ul');
+  addListItem(menuList , "Contanc")
+  addListItem(menuList , "info")
+  addListItem(menuList ,"about")
+  menu.appendChild(menuList);
+}
+function addListItem(menuList ,listText) {
+  const listItem = document.createElement('li');
+  const text = document.createTextNode(listText);
+  listItem.appendChild(text);
+  menuList.appendChild(listItem);
+}
+function display() {
+  const menu = document.createElement('div');
+  menu.classList.add('mobileMenuDiv');
+  document.body.appendChild(menu);
+  createList(menu);
+
+}
+hamburgerButton.addEventListener('click', display);
