76e250760ae95439754f05d16fddf3fa06671ec6
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-06-19 18:06:40 -0300

    add mobileExitButton styles - add list Anchor

--- mobileMenu.js
+++ mobileMenu.js
@@ -1,29 +1,34 @@
 const hamburgerButton = document.querySelector('#mobileMenuBtn');
-
 function removeMenu() {
   const menu = document.querySelector('.mobileMenuDiv');
   menu.remove();
 }
-function addListItem(menuList, listText) {
+function addListItem(menuList, listText, gotToSection) {
   const listItem = document.createElement('li');
+  const listItemAnchor = document.createElement('a')
   const text = document.createTextNode(listText);
-  listItem.classList.add('mobileMenulistItem')
+  listItem.classList.add('mobileMenulistItem');
   listItem.addEventListener('click', removeMenu);
-  listItem.appendChild(text);
+  listItemAnchor.href = gotToSection;
+  listItemAnchor.appendChild(text)
+  listItem.appendChild(listItemAnchor);
   menuList.appendChild(listItem);
 }
-function createList(menu) {
+function createMenuElements(menu) {
+  const mobileExitButton = document.createElement('button')
+  mobileExitButton.classList.add('mobileExitButton')
   const menuList = document.createElement('ul');
-  menuList.classList.add('mobileMenuList')
-  addListItem(menuList, ' Portfolio');
-  addListItem(menuList, 'About');
-  addListItem(menuList, 'Contact');
+  menuList.classList.add('mobileMenuList');
+  addListItem(menuList, 'Portfolio', '#myWorks');
+  addListItem(menuList, 'About', '#about');
+  addListItem(menuList, 'Contact', '#contact');
+  menu.appendChild(mobileExitButton);
   menu.appendChild(menuList);
 }
 function display() {
   const menu = document.createElement('div');
   menu.classList.add('mobileMenuDiv');
   document.body.appendChild(menu);
-  createList(menu);
+  createMenuElements(menu);
 }
 hamburgerButton.addEventListener('click', display);
