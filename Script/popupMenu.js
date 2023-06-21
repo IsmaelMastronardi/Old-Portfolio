63bc1c040edbd6fefd57c2dca65eb71420992267
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-06-21 01:49:51 -0300

    add popmenu values with javascript

--- Script/popupMenu.js
+++ Script/popupMenu.js
@@ -1,10 +1,14 @@
+
 const works = [{
   name: 'Multi-Post Stories Gain+Glory',
   nameMenu: 'Keeping track of hundreds of components',
-  description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
-  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
+  description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+    when an unknown printer took a galley of type and scrambled it 1960s. 
+    <br class="popupMenuMobileBreak"/><br class="popupMenuMobileBreak"/>
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+    when an unknown printer took a galley of type and scrambled it 1960s.
   `,
   descriptionB: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
@@ -14,6 +18,7 @@ const works = [{
   featuredImage: './assets/images/popupMenu/popUpMenuMobile.png',
   featureImageDesktop: './assets/images/popupMenu/popUpMenuDesktop.png',
   technologies: ['Ruby on Rails', 'CSS', 'javaScript', 'HTML'],
+  technologiesDesktop : ['Codekit','Github','JavaScript','Bootstrap','Terminal','Codepen'],
   'link to live version': '',
   'link to source': '',
   identification: '#work1',
@@ -34,6 +39,7 @@ const works = [{
   featuredImage: './assets/images/popupMenu/popUpMenuMobile.png',
   featureImageDesktop: './assets/images/popupMenu/popUpMenuDesktop.png',
   technologies: ['Ruby on Rails', 'CSS', 'javaScript', 'HTML'],
+  technologiesDesktop : ['Codekit','Github','JavaScript','Bootstrap','Terminal','Codepen'],
   'link to live version': '',
   'link to source': '',
   identification: '#work2',
@@ -54,6 +60,7 @@ const works = [{
   featuredImage: './assets/images/popupMenu/popUpMenuMobile.png',
   featureImageDesktop: './assets/images/popupMenu/popUpMenuDesktop.png',
   technologies: ['Ruby on Rails', 'CSS', 'javaScript', 'HTML'],
+  technologiesDesktop : ['Codekit','Github','JavaScript','Bootstrap','Terminal','Codepen'],
   'link to live version': '',
   'link to source': '',
   identification: '#work3',
@@ -74,6 +81,7 @@ const works = [{
   featuredImage: './assets/images/popupMenu/popUpMenuMobile.png',
   featureImageDesktop: './assets/images/popupMenu/popUpMenuDesktop.png',
   technologies: ['Ruby on Rails', 'CSS', 'javaScript', 'HTML'],
+  technologiesDesktop : ['Codekit','Github','JavaScript','Bootstrap','Terminal','Codepen'],
   'link to live version': '',
   'link to source': '',
   identification: '#work4',
@@ -94,6 +102,7 @@ const works = [{
   featuredImage: './assets/images/popupMenu/popUpMenuMobile.png',
   featureImageDesktop: './assets/images/popupMenu/popUpMenuDesktop.png',
   technologies: ['Ruby on Rails', 'CSS', 'javaScript', 'HTML'],
+  technologiesDesktop : ['Codekit','Github','JavaScript','Bootstrap','Terminal','Codepen'],
   'link to live version': '',
   'link to source': '',
   identification: '#work5',
@@ -114,6 +123,7 @@ const works = [{
   featuredImage: './assets/images/popupMenu/popUpMenuMobile.png',
   featureImageDesktop: './assets/images/popupMenu/popUpMenuDesktop.png',
   technologies: ['Ruby on Rails', 'CSS', 'javaScript', 'HTML'],
+  technologiesDesktop : ['Codekit','Github','JavaScript','Bootstrap','Terminal','Codepen'],
   linkToLiveVersion: '',
   linkToSource: '',
   identification: '#work6',
@@ -147,27 +157,45 @@ function createWorkArticle(work) {
 
 window.onload = works.forEach(createWorkArticle);
 
-function createPopupMenu() {
+function createPopupMenu(obj) {
   const popupMenu = document.querySelector('#popupMenu');
   popupMenu.classList.remove('alternateDisplay');
-
-
-
-
-  
+  const popupMobileImg = document.querySelector('#popupMobileImg');
+  popupMobileImg.src = obj['featuredImage'];
+  const popupDesktopImg = document.querySelector('#popupDesktopImg');
+  popupDesktopImg.src = obj['featureImageDesktop'];
+  const popupMenuTitle = document.querySelector('#popupMenuTitle');
+  popupMenuTitle.innerText = obj['nameMenu'];
+  const mobileMenuBtn1 = document.querySelector('#mobileMenuBtn1');
+  mobileMenuBtn1.innerText = obj['technologies'][0];
+  const mobileMenuBtn2 = document.querySelector('#mobileMenuBtn2');
+  mobileMenuBtn2.innerText = obj['technologies'][1];
+  const mobileMenuBtn3 = document.querySelector('#mobileMenuBtn3');
+  mobileMenuBtn3.innerText = obj['technologies'][2];
+  const desktopMenuBtn1 = document.querySelector('#desktopMenuBtn1');
+  desktopMenuBtn1.innerText = obj['technologiesDesktop'][0];
+  const desktopMenuBtn2 = document.querySelector('#desktopMenuBtn2');
+  desktopMenuBtn2.innerText = obj['technologiesDesktop'][1];
+  const desktopMenuBtn3 = document.querySelector('#desktopMenuBtn3');
+  desktopMenuBtn3.innerText = obj['technologiesDesktop'][2];
+  const desktopMenuBtn4 = document.querySelector('#desktopMenuBtn4');
+  desktopMenuBtn4.innerText = obj['technologiesDesktop'][3];
+  const desktopMenuBtn5 = document.querySelector('#desktopMenuBtn5');
+  desktopMenuBtn5.innerText = obj['technologiesDesktop'][4];
+  const desktopDescription = document.querySelector('#mobilePopupDescription');
+  desktopDescription.innerHTML = obj['description'];
 }
 
 function deletePopupMenu() {
   const popupMenu = document.querySelector('#popupMenu');
   popupMenu.classList.add('alternateDisplay')
 }
-
-const seeBtn = document.getElementById(works[0].identification);
-
-seeBtn.addEventListener('click', () => {
-  createPopupMenu();
-});
-
+works.forEach((work) =>{
+  const seeProject = document.getElementById(work.identification);
+  seeProject.addEventListener('click', () => {
+    createPopupMenu(work);
+  });
+})
 const popupMenuExitBtn = document.querySelector('#popupMenuExitBtn');
 popupMenuExitBtn.addEventListener('click', ()=>{
   deletePopupMenu()
