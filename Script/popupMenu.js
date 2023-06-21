246cc155426eb858be1b77c00f00ba0cc4b11989
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-06-21 16:24:48 -0300

    delete popup menu from html

--- Script/popupMenu.js
+++ Script/popupMenu.js
@@ -170,23 +170,66 @@ function createWorkArticle(work) {
 
 window.onload = works.forEach(createWorkArticle);
 
+function deletePopupMenu() {
+  const menu = document.querySelector('#newMenu');
+  document.body.removeChild(menu);
+  document.body.style.overflow = 'auto';
+}
 function createPopupMenu(obj) {
-  const popupMenu = document.querySelector('#popupMenu');
-  popupMenu.classList.remove('alternateDisplay');
-  const popupMobileImg = document.querySelector('#popupMobileImg');
-  popupMobileImg.src = obj.featuredImage;
-  const popupDesktopImg = document.querySelector('#popupDesktopImg');
-  popupDesktopImg.src = obj.featureImageDesktop;
-  const popupMenuTitle = document.querySelector('#popupMenuTitle');
-  popupMenuTitle.innerText = obj.nameMenu;
-  const seeLiveDesktop = document.querySelector('#seeLiveDesktop');
-  seeLiveDesktop.href = obj['link to live version'];
-  const seeSourceDesktop = document.querySelector('#seeSourceDesktop');
-  seeSourceDesktop.href = obj['link to source'];
-  const seeLiveMobile = document.querySelector('#seeLiveMobile');
-  seeLiveMobile.href = obj['link to live version'];
-  const seeSourceMobile = document.querySelector('#seeSourceMobile');
-  seeSourceMobile.href = obj['link to source'];
+  const menu = document.createElement('div');
+  menu.id = 'newMenu';
+  menu.innerHTML += `
+  <section class="popupMenuBackground" id="popupMenu">
+      <article class="popupMenuDiv">
+        <img src="./assets/images/popupMenu/Disabled.png" class="popupMenuExitBtn" id="popupMenuExitBtn">
+        <div style="position: relative;">
+          <button  class="popupMenuMobileExitBtn" id="popupMenuMobileExitBtn">
+            <img src="./assets/images/popupMenu/Icon.png">
+          </button>
+          <img  src=${obj.featuredImage} class="popupMobilePic" id="popupMobileImg">
+        </div>
+        <img src="${obj.featureImageDesktop}" class="popupMobilePicDesktop" id="popupDesktopImg">
+        <div class="popupMenuTitleDiv">
+          <h3 class="popupMenuTitle" id="popupMenuTitle">${obj.nameMenu}</h3>
+          <ul class="greyProjectButtons popupListDesktop popupListDesktopGreen">
+            <li class="LinkButton popupMenubtn" >
+              <a href="${obj['link to live version']}" class="LinkButton popupMenubtn" target="_blank" id="seeLiveDesktop">
+                See live
+                <img src="./assets/images/popupMenu/seeLiveBtn.png" class="seeLiveBtn">
+              </a>         
+            </li>
+            <li class="LinkButton  popupMenuB" >
+              <a href="${obj['link to source']}" class="LinkButton  popupMenuB" target="_blank" id="seeSourceDesktop">
+                See Source
+                <img src="./assets/images/popupMenu/seeSourceBtn.png" class="seeSourceBtn">
+              </a>
+            </li>     
+          </ul>
+        </div>
+        <ul class="greyProjectButtons popupListMobile greyList" id="mobileBtnList">
+        </ul>
+        <ul class="greyProjectButtons popupListDesktop greyList" id="desktopBtnList">   
+        </ul>
+        <p class="popupMobileDescription" id="mobilePopupDescription">${obj.description}
+        </p>
+        <ul class="greyProjectButtons popupListMobile ">
+          <li class="LinkButton">
+            <a href="${obj['link to live version']}"  class="LinkButton" target="_blank" id="seeLiveMobile">
+              See live
+              <img src="./assets/images/popupMenu/seeLiveBtn.png" class="seeLiveBtn">
+            </a>
+          </li>
+          <li class="LinkButton">
+            <a href="${obj['link to source']}" class="LinkButton" target="_blank" id="seeSourceMobile">
+              See Source
+              <img src="./assets/images/popupMenu/seeSourceBtn.png" class="seeSourceBtn">
+            </a>
+          </li>
+        </ul>
+      </article>
+    </section>
+  `;
+  document.body.appendChild(menu);
   const mobileBtnList = document.querySelector('#mobileBtnList');
   for (let x = 0; x < obj.technologies.length - 1; x += 1) {
     const mobileMenuBtn = document.createElement('li');
@@ -199,35 +242,20 @@ function createPopupMenu(obj) {
     desktopMenuBtn.innerHTML += `<li class="projectListItems" >${obj.technologiesDesktop[i]}</li>`;
     desktopBtnList.appendChild(desktopMenuBtn);
   }
-  const desktopDescription = document.querySelector('#mobilePopupDescription');
-  desktopDescription.innerHTML = obj.description;
   document.body.style.overflow = 'hidden';
+  const popupMenuExitBtn = document.querySelector('#popupMenuExitBtn');
+  popupMenuExitBtn.addEventListener('click', () => {
+    deletePopupMenu();
+  });
+  const popupMenuMobileExitBtn = document.querySelector('#popupMenuMobileExitBtn');
+  popupMenuMobileExitBtn.addEventListener('click', () => {
+    deletePopupMenu();
+  });
 }
 
-function deletePopupMenu() {
-  const popupMenu = document.querySelector('#popupMenu');
-  popupMenu.classList.add('alternateDisplay');
-  const desktopBtnList = document.querySelector('#desktopBtnList');
-  while (desktopBtnList.firstChild) {
-    desktopBtnList.removeChild(desktopBtnList.firstChild);
-  }
-  const mobileBtnList = document.querySelector('#mobileBtnList');
-  while (mobileBtnList.firstChild) {
-    mobileBtnList.removeChild(mobileBtnList.firstChild);
-  }
-  document.body.style.overflow = 'auto';
-}
 works.forEach((work) => {
   const seeProject = document.getElementById(work.identification);
   seeProject.addEventListener('click', () => {
     createPopupMenu(work);
   });
-});
-const popupMenuExitBtn = document.querySelector('#popupMenuExitBtn');
-popupMenuExitBtn.addEventListener('click', () => {
-  deletePopupMenu();
-});
-const popupMenuMobileExitBtn = document.querySelector('#popupMenuMobileExitBtn');
-popupMenuMobileExitBtn.addEventListener('click', () => {
-  deletePopupMenu();
 });
