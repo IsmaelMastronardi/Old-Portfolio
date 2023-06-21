ebdfcca979c3a693c55143fb5b0a11541eceb5b8
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-06-20 22:16:51 -0300

    add html for popUp menu

--- Script/popupMenu.js
+++ Script/popupMenu.js
@@ -1,84 +1,162 @@
-
-const Works = {
-    name :"",
-    description :"",
-    'featured image' :"",
-    technologies :"",
-    'link to live version' :"",
-    'link to source' :""
-}
-
-const newWork = Object.create(Works);
-newWork['name'] = "Multi-Post Stories Gain+Glory";
-newWork['description'] = "una descripcion";
-newWork['feature image'] = "hola";
-newWork['technologies'] = ["Ruby on Rails" , "CSS" , "javaScript" , "HTML"];
-newWork['link to live version'] = "no link yet";
-newWork['link to source'] = "also no link yet";
+const works = [{
+  name: 'Multi-Post Stories Gain+Glory',
+  nameMenu: 'Keeping track of hundreds of components',
+  description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
+  `,
+  descriptionB: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+   when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum 
+   is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+   standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+  featuredImage: './assets/images/popupMenu/popUpMenuMobile.png',
+  featureImageDesktop: './assets/images/popupMenu/popUpMenuDesktop.png',
+  technologies: ['Ruby on Rails', 'CSS', 'javaScript', 'HTML'],
+  'link to live version': '',
+  'link to source': '',
+  identification: '#work1',
+},
+{
+  name: 'Multi-Post Stories Gain+Glory',
+  nameMenu: 'Keeping track of hundreds of components',
+  description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
+  `,
+  descriptionB: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+   when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum 
+   is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+   standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+  featuredImage: './assets/images/popupMenu/popUpMenuMobile.png',
+  featureImageDesktop: './assets/images/popupMenu/popUpMenuDesktop.png',
+  technologies: ['Ruby on Rails', 'CSS', 'javaScript', 'HTML'],
+  'link to live version': '',
+  'link to source': '',
+  identification: '#work2',
+},
+{
+  name: 'Multi-Post Stories Gain+Glory',
+  nameMenu: 'Keeping track of hundreds of components',
+  description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
+  `,
+  descriptionB: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+   when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum 
+   is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+   standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+  featuredImage: './assets/images/popupMenu/popUpMenuMobile.png',
+  featureImageDesktop: './assets/images/popupMenu/popUpMenuDesktop.png',
+  technologies: ['Ruby on Rails', 'CSS', 'javaScript', 'HTML'],
+  'link to live version': '',
+  'link to source': '',
+  identification: '#work3',
+},
+{
+  name: 'Multi-Post Stories Gain+Glory',
+  nameMenu: 'Keeping track of hundreds of components',
+  description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
+  `,
+  descriptionB: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+   when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum 
+   is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+   standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+  featuredImage: './assets/images/popupMenu/popUpMenuMobile.png',
+  featureImageDesktop: './assets/images/popupMenu/popUpMenuDesktop.png',
+  technologies: ['Ruby on Rails', 'CSS', 'javaScript', 'HTML'],
+  'link to live version': '',
+  'link to source': '',
+  identification: '#work4',
+},
+{
+  name: 'Multi-Post Stories Gain+Glory',
+  nameMenu: 'Keeping track of hundreds of components',
+  description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
+  `,
+  descriptionB: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+   when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum 
+   is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+   standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+  featuredImage: './assets/images/popupMenu/popUpMenuMobile.png',
+  featureImageDesktop: './assets/images/popupMenu/popUpMenuDesktop.png',
+  technologies: ['Ruby on Rails', 'CSS', 'javaScript', 'HTML'],
+  'link to live version': '',
+  'link to source': '',
+  identification: '#work5',
+},
+{
+  name: 'Multi-Post Stories Gain+Glory',
+  nameMenu: 'Keeping track of hundreds of components',
+  description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.
+  `,
+  descriptionB: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+   when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum 
+   is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+   standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
+  featuredImage: './assets/images/popupMenu/popUpMenuMobile.png',
+  featureImageDesktop: './assets/images/popupMenu/popUpMenuDesktop.png',
+  technologies: ['Ruby on Rails', 'CSS', 'javaScript', 'HTML'],
+  linkToLiveVersion: '',
+  linkToSource: '',
+  identification: '#work6',
+},
+];
 
 const workSection = document.querySelector('#newWorkSection');
-const workDiv = document.querySelector('#newWorkDiv')
+const workDiv = document.querySelector('#newWorkDiv');
 
-
-function createWorkArticle (work){
-const workArticle = document.createElement('article')
-workArticle.classList.add('project');
-
-const projectPictureDiv = document.createElement('div');
-projectPictureDiv.classList.add('projectPicture')
-
-const projectInfoDiv = document.createElement('div');
-projectInfoDiv.classList.add('projectInfo');
-const projectTitle = document.createElement('h3');
-projectTitle.classList.add('projectTitle');
-projectTitle.innerText = work['name'];
-const projectBtnList = document.createElement('ul');
-projectBtnList.classList.add('greyProjectButtons');
-
-const projectListItems = document.createElement('projectListItems');
-projectListItems.classList.add('projectListItems');
-projectListItems.innerText = work['technologies'][0];
-const projectListItems2 = document.createElement('projectListItems');
-projectListItems2.classList.add('projectListItems');
-projectListItems2.innerText = work['technologies'][1];
-const projectListItems3 = document.createElement('projectListItems');
-projectListItems3.classList.add('projectListItems');
-projectListItems3.innerText = work['technologies'][2];
-const projectListItems4 = document.createElement('projectListItems');
-projectListItems4.classList.add('projectListItems');
-projectListItems4.innerText = work['technologies'][3];
-
-const projectGreenBtn = document.createElement('div');
-projectGreenBtn.classList.add('greenProjectButton');
-const seeProjectBtn = document.createElement('button');
-seeProjectBtn.classList.add('LinkButton');
-seeProjectBtn.innerText = "See Project";
-
-
-workDiv.appendChild(workArticle);
-workArticle.appendChild(projectPictureDiv)
-workArticle.appendChild(projectInfoDiv)
-projectInfoDiv.appendChild(projectTitle)
-projectInfoDiv.appendChild(projectBtnList)
-projectInfoDiv.appendChild(projectGreenBtn)
-
-
-
-projectBtnList.appendChild(projectListItems)
-projectBtnList.appendChild(projectListItems2)
-projectBtnList.appendChild(projectListItems3)
-projectBtnList.appendChild(projectListItems4)
-projectGreenBtn.appendChild(seeProjectBtn)
-
-
-
-
+function createWorkArticle(work) {
+  const workArticle = document.createElement('article');
+  workArticle.classList.add('project');
+  workArticle.innerHTML += `
+  <div class="projectPicture">
+    <img />
+  </div>
+  <div class="projectInfo">
+    <h3 class="projectTitle">Multi-Post Stories <br />Gain+Glory</h3>
+    <ul class="greyProjectButtons">
+      <li class="projectListItems">${work.technologies[0]}</li>
+      <li class="projectListItems">${work.technologies[1]}</li>
+      <li class="projectListItems">${work.technologies[2]}</li>
+      <li class="projectListItems">${work.technologies[3]}</li>
+    </ul>
+    <div class="greenProjectButton">
+      <button class="LinkButton" id="${work.identification}" >See project</button>
+    </div>
+  </div>`;
+  workDiv.appendChild(workArticle);
 }
 
-window.onload = createWorkArticle(newWork);
-window.onload = createWorkArticle(newWork);
-window.onload = createWorkArticle(newWork);
-window.onload = createWorkArticle(newWork);
-window.onload = createWorkArticle(newWork);
-window.onload = createWorkArticle(newWork);
+window.onload = works.forEach(createWorkArticle);
 
+function createPopupMenu() {
+  const popupMenu = document.createElement('div');
+  popupMenu.innerHTML += `
+
+  `;
+  document.body.appendChild(popupMenu)
+}
+
+const seeBtn = document.getElementById(works[0].identification);
+
+seeBtn.addEventListener('click', () => {
+  createPopupMenu();
+});
